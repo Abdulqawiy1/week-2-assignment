f7b8fb7baed7fba596e70f5b1752b322de0a7b34
Abdulqawiy <abdulqawiyqassim@gmail.com>
2025-09-17 01:52:04 +0100

    week 2 assignment

---
+++ my_list.py
@@ -0,0 +1,30 @@
+#Empty List
+my_list = []
+
+#Appending some elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+my_list.insert(1,15)
+print(my_list)
+
+#extending the list
+my_list.extend([50])
+my_list.extend([60])
+my_list.extend([70])
+print(my_list)
+
+#removing the last element
+my_list.remove(70)
+print(my_list)
+
+#ascending order
+my_list.sort()
+print(my_list)
+
+#Finding a value
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
